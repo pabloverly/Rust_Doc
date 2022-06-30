72e08c6fba64b2e6508ad3145d9dfa1506996cb3
pabloverly <root@vps.server.local>
2022-06-30 12:04:38 +0000

    30-06-2022

---
+++ target/debug/deps/estudo-0e1f1b66d9224b89.d
@@ -0,0 +1,5 @@
+c:\P43LO\6_DEV\RUST\Rust_Doc\target\debug\deps\estudo-0e1f1b66d9224b89.rmeta: src\main.rs
+
+c:\P43LO\6_DEV\RUST\Rust_Doc\target\debug\deps\estudo-0e1f1b66d9224b89.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/estudo-518ce6f35b5c3de5.d
@@ -0,0 +1,5 @@
+C:\P43LO\6_DEV\RUST\Rust_Doc\target\debug\deps\estudo-518ce6f35b5c3de5.exe: src\main.rs
+
+C:\P43LO\6_DEV\RUST\Rust_Doc\target\debug\deps\estudo-518ce6f35b5c3de5.d: src\main.rs
+
+src\main.rs:
